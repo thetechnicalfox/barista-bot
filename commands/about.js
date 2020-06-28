63e4ed2fcb94cc8fbf2c8282e54ff7873ff89cc2
TheTechnicalFox <thetechnicalfox@gmail.com>
2020-06-27 23:12:47 -0400

    Update embed

--- commands/about.js
+++ commands/about.js
@@ -8,15 +8,15 @@ module.exports = {
     description: 'Explains the purpose of the bot and provides links',
     execute (message, args) {
         const messageEmbed = new Discord.MessageEmbed()
-            .setColor('FF0000')
+            .setColor('386895')
             .setTitle('Barista Bot')
             .setURL('https://github.com/thetechnicalfox/barista-bot')
-            .setDescription('Just a simple, multi-purpose Discord Bot')
+            .setDescription('Originating to provide a starting ground in the world of server-side JavaScript, Barista Bot serves as a perfect example of how engaging and educational simple programming projects can be. All code is publicly available on GitHub in the link above for comments and contribution.')
             .setThumbnail('https://i.imgur.com/WtJZ3Wk.png')
+            .addField('Version', '1.0', true)
             .addFields(
-                {name: 'About', value: 'Originating to provide a starting ground in the world of server-side JavaScript, Barista Bot serves as a perfect example of how engaging and educational simple programming projects can be. All code is publicly available on GitHub in the link above for comments and contribution.'},
-                {name: 'Version', value: '1.0', inline: true },
-                {name: 'Maintained by', value: 'TheTechnicalFox#0056', inline: true } )
+                {name: 'Maintained by', value: 'TheTechnicalFox#0056', inline: true },
+                {name: 'Commands', value: 'b.commands', inlune: true} )
             .setTimestamp()
             .setFooter('Barista Bot', 'https://i.imgur.com/WtJZ3Wk.png');
 
