5699e6600c48b6aa239c02a4aaa7e8a446729abd
TheTechnicalFox <thetechnicalfox@gmail.com>
2020-07-01 22:35:37 -0400

    Update to support global embeds

--- commands/say.js
+++ commands/say.js
@@ -1,5 +1,6 @@
 const { Channel, DiscordAPIError } = require("discord.js")
 const Discord = require('discord.js');
+const embeds = require('../modules/embeds.js');
 
 module.exports = {
     name: 'say',
@@ -11,30 +12,14 @@ module.exports = {
     execute (message, args) {
         const self = message.client;
         if (!message.guild.me.hasPermission('MANAGE_MESSAGES')) {
-            return message.channel.send({embed: {
-                color: 'FF0000',
-                title: 'Error',
-                description: `Cannot perform action: Missing manage messages permission.`,
-                timestamp: new Date(),
-                footer: {
-                    icon_url: 'https://i.imgur.com/WtJZ3Wk.png',
-                    text: 'Barista Bot'
-                }
-            }});
+            return message.channel.send(embeds.newErrorEmbed('Cannot perform action: Missing permission \`Manage Messages\`'))
         }
-        contentError = new Discord.MessageEmbed()
-            .setColor('FF0000')
-            .setTitle('Error')
-            .setDescription('Please provide message content.')
-            .setTimestamp()
-            .setFooter('Barista Bot', 'https://i.imgur.com/WtJZ3Wk.png');
-
         // If channel-mention is provided:
         let channel = args[0];
         if (channel.startsWith('<#') && channel.endsWith('>')) {
             let channelID = channel.slice(2,20);
             let content = args.slice(1).join(' ');
-            if (content.length === 0) return message.channel.send(contentError);
+            if (content.length === 0) return message.channel.send(embed.newErrorEmbed('Please provide a message to send.'));
             self.channels.cache.get(channelID).send(content);
             message.delete()
         } else {
