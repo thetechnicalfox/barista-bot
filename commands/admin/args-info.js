829ebf710c972d9f5168379952b6ce6a0846e127
TheTechnicalFox <thetechnicalfox@gmail.com>
2020-09-24 16:50:53 -0400

    Change <> to {}

--- commands/admin/args-info.js
+++ commands/admin/args-info.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'args-info',
     description: 'Restates the provided arguments and their length.',
     args: true,
-    usage: `<arguments>`,
+    usage: `{arguments}`,
     execute(message, args) {
           message.channel.send(`Arguments: ${args} length: ${args.length}`);
     }
