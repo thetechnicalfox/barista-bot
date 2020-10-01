285a153b25af7aed9a4ec35a79a7cbd706cf18ee
TheTechnicalFox <thetechnicalfox@gmail.com>
2020-10-01 11:53:57 -0400

    Add guild events to events handler

---
+++ events/guildCreate.js
@@ -0,0 +1,3 @@
+module.exports  = (self, guild) => {
+    console.log(`Bot has been added to ${guild.name} with id of ${guild.id} and ${guild.memberCount} members.`);
+}
---
+++ events/guildDelete.js
@@ -0,0 +1,3 @@
+module.exports  = (self, guild) => {
+    console.log(`Bot has been removed from "${guild.name}" with id of ${guild.id} and ${guild.memberCount} members.`);
+}
